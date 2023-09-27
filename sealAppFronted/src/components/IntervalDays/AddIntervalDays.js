53de32edf5836146da2c4ea1f0d60a81a6fbb8b6
ShopifyDadSupport <webdeveloper@unimedint.com>
2023-09-27 14:53:34 -0700

    add loader in intervaldays component

--- sealAppFronted/src/components/IntervalDays/AddIntervalDays.js
+++ sealAppFronted/src/components/IntervalDays/AddIntervalDays.js
@@ -1,26 +1,33 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './AddIntervalDays.css';
 import axios from 'axios';
+import { Dna } from 'react-loader-spinner';
 function TagsInput() {
     const [tags, setTags] = useState([]);
     const [inputValue, setInputValue] = useState('');
-
+    const [isLoading, setIsLoading] = useState(true);
     const [currentURL, setCurrentURL] = useState('');
-    const [data, setData] = useState([]); 
+    const [data, setData] = useState([]);
     useEffect(() => {
         setCurrentURL(window.location.hostname); // Get the current URL when the component mounts
         localStorage.setItem("tempStoreName", window.location.hostname);
 
-            // Fetch data from the API
-            fetch('https://sealapp-6ptb.onrender.com/getadd/addIntervalDays')
-              .then(response => response.json())
-              .then(data => setData(data.subscription_interval_days))
-              .catch(error => console.error('Error fetching data:', error));
+        // Fetch data from the API
+        fetch('https://sealapp-6ptb.onrender.com/getadd/addIntervalDays')
+            .then(response => response.json())
+            .then(data => {
+                setData(data.subscription_interval_days)
+                setIsLoading(false);
+            })
+            .catch(error => {
+                setIsLoading(false);
+                console.error('Error fetching data:', error)
+            });
     }, []);
 
     function handleKeyDown(e) {
         if (e.key === 'Enter') {
-            e.preventDefault(); 
+            e.preventDefault();
             addTag();
         }
     }
@@ -34,23 +41,23 @@ function TagsInput() {
 
     // const removeTag = (index) => {
     //     // console.log("Trying to remove tag at index:", index);
-    
+
     //     // const updatedTags = [...tags];
     //     // updatedTags.splice(index, 1);
     //     // setTags(updatedTags);
-     
+
     // };
-    
-        const removeTag = (tag, index) => {
-            console.log("Removing tag:", tag);
-            // setTags(tag);
-            axios.post('https://sealapp-6ptb.onrender.com/remove/addIntervaldays', 
-              { 
+
+    const removeTag = (tag, index) => {
+        console.log("Removing tag:", tag);
+        // setTags(tag);
+        axios.post('https://sealapp-6ptb.onrender.com/remove/addIntervaldays',
+            {
                 tag: tag,
                 url: currentURL
-                }) // Adjust the API endpoint as needed
+            }) // Adjust the API endpoint as needed
             .then(response => {
-                console.log('Tag remove successfully:', response.data,currentURL);
+                console.log('Tag remove successfully:', response.data, currentURL);
                 const updatedTags = [...tags];
                 const removeUpdateData = updatedTags.splice(index, 1);
                 setTags(updatedTags);
@@ -60,33 +67,33 @@ function TagsInput() {
             .catch(error => {
                 console.error('Error adding tag:', error);
             });
-        };
+    };
 
     const addTag = () => {
         // if (inputValue.trim()) {
         //     setTags([...tags, inputValue]);
         //     setInputValue('');
         // }
-        axios.post('https://sealapp-6ptb.onrender.com/add/addIntervalDays', 
-        { 
-            tag: inputValue,
-            url: currentURL
-         }) // Adjust the API endpoint as needed
-        .then(response => {
-            console.log('Tag added successfully:', response.data,currentURL);
-            setTags([...tags, inputValue]);
-            setInputValue('');
-        })
-        .catch(error => {
-            console.error('Error adding tag:', error);
-        });
+        axios.post('https://sealapp-6ptb.onrender.com/add/addIntervalDays',
+            {
+                tag: inputValue,
+                url: currentURL
+            }) // Adjust the API endpoint as needed
+            .then(response => {
+                console.log('Tag added successfully:', response.data, currentURL);
+                setTags([...tags, inputValue]);
+                setInputValue('');
+            })
+            .catch(error => {
+                console.error('Error adding tag:', error);
+            });
     };
     const allTags = [...data, ...tags];
     return (
         <div className='interval_days__content'>
             <h2 className='interval__days'>Add Interval Days</h2>
             <div className='ineterval__days__content'>
-            <input
+                <input
                     value={inputValue}
                     onChange={(e) => setInputValue(e.target.value)}
                     onKeyDown={handleKeyDown}
@@ -95,14 +102,29 @@ function TagsInput() {
                     placeholder="Enter the inerval days...."
                 />
                 <button onClick={addTag} className="add-button">Add</button>
-                </div>
+            </div>
             <div className="tags-input-container">
-                {allTags.map((tag, index) => (
-                    <div className="tag-item" key={index}>
-                        <span className="text">{tag}</span>
-                        <span className="close" onClick={() => removeTag(tag,index)}>&times;</span>
-                    </div>
-                ))}
+
+                {isLoading && (
+                    <Dna
+                        visible={true}
+                        height={80}
+                        width={80}
+                        ariaLabel="dna-loading"
+                        wrapperStyle={{}}
+                        wrapperClass="dna-wrapper"
+                    />
+                )}
+                {!isLoading && (
+                    <>
+                        {allTags.map((tag, index) => (
+                            <div className="tag-item" key={index}>
+                                <span className="text">{tag}</span>
+                                <span className="close" onClick={() => removeTag(tag, index)}>&times;</span>
+                            </div>
+                        ))}
+                    </>
+                )}
 
             </div>
         </div>
