7ff06bf5364340faa0ef676e42af853e9c925d50
ShopifyDadSupport <webdeveloper@unimedint.com>
2023-10-11 09:17:29 -0700

    build folder update

--- sealAppFronted/src/components/Pagination/SubscriptionCancelOrder.js
+++ sealAppFronted/src/components/Pagination/SubscriptionCancelOrder.js
@@ -14,6 +14,7 @@ import { Dna } from 'react-loader-spinner';
 let PageSize = 8;
 
 export default function PaginationApp() {
+  var concatenatedValue;
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
   const [data, setData] = useState([]); // State to hold the fetched data
@@ -109,7 +110,7 @@ export default function PaginationApp() {
               {currentTableData.length > 0 ? (
                 currentTableData.map((item) => (
                   <tr key={item.id}>
-                    <td>{item.subscription_order_name}</td>
+                    <td>#{ concatenatedValue = item.id + 1000}</td>
                     <td>{item.subscription_customer_name}</td>
                     <td>{item.subscription_customer_email}</td>
                     {/* <td>{formatDate(item.create_order_date)}</td> */}
--- sealAppFronted/src/components/Pagination/SubscriptionUppcoming.js
+++ sealAppFronted/src/components/Pagination/SubscriptionUppcoming.js
@@ -6,6 +6,7 @@ import './PaginationApp.css'; // Assuming this is for styling
 const PageSize = 10;
 
 export default function SubscriptionUpcoming() {
+  var concatenatedValue;
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
   const [data, setData] = useState([]); // State to hold the fetched data
@@ -82,7 +83,7 @@ export default function SubscriptionUpcoming() {
               {currentTableData.length > 0 ? (
                 currentTableData.map((item) => (
                   <tr key={item.id}>
-                    <td>{item.subscription_order_name}</td>
+                    <td>#{ concatenatedValue = item.id + 1000}</td>
                     <td>{item.subscription_customer_name}</td>
                     <td>{item.subscription_customer_email}</td>
                     <td>{item.Next_Shipment_Date}</td>
